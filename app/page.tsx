5dc82feca46b1857b6488c91a640cb17e62df364
Zack Hanni <zack.hannI@gmail.com>
2024-04-22 11:56:11 -0400

    removed padding

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import ConnectToMedia from "./components/ConnectToMedia";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <ConnectToMedia />
     </main>
   );
