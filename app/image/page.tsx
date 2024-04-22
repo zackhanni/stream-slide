ad16c7a248f0e55166e85411c3739476c33c84a5
Zack Hanni <zack.hannI@gmail.com>
2024-04-22 11:56:02 -0400

    removed note

--- app/image/page.tsx
+++ app/image/page.tsx
@@ -4,7 +4,6 @@ import ImagePlayer from "../components/ImagePlayer";
 export default function page() {
   return (
     <main>
-      {/* add in overlay to adjust settings like transition speed, screen text, play/pause, next, previous, and END/goback. overlay should disapear when mouse stops moving */}
       <ImagePlayer />
     </main>
   );
